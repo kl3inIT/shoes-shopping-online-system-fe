3bb729b63015f9b30d46e065d725ef042682b45d
kl3inIT <dathip04@gmail.com>
2026-01-18 19:01:25 +0700

    add lint staged

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -11,13 +11,20 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tseslint.configs.recommendedTypeChecked,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      reactX.configs['recommended-typescript'],
+      reactDom.configs.recommended,
+      prettier,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'lastest',
       globals: globals.browser,
     },
+    parserOptions: {
+      project: ['./tsconfig.app.json', './tsconfig.node.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
-])
+]);
